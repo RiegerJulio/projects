02b5143ebca6572f82103f6952436149e88f1aef
RiegerJulio <riegerproducoes@gmail.com>
2022-02-23 11:44:06 -0300

    login css pc e tablet

--- 13-project-trivia-react-redux/src/pages/Settings.js
+++ 13-project-trivia-react-redux/src/pages/Settings.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import './css/settings.css';
+
 class Settings extends React.Component {
   render() {
     return (
-      <h1 data-testid="settings-title">Configurações</h1>
+      <div className="config-container">
+        <h1 data-testid="settings-title" className="config-message">Em construção</h1>
+        <div className="preloader-wrapper big active">
+          <div className="spinner-layer spinner-blue-only">
+            <div className="circle-clipper left">
+              <div className="circle" />
+            </div>
+            <div className="gap-patch">
+              <div className="circle" />
+            </div>
+            <div className="circle-clipper right">
+              <div className="circle" />
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
