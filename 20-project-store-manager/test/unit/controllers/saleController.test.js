d5755d30ecaf188c114b1c70ad5e482622e14150
RiegerJulio <riegerproducoes@gmail.com>
2022-04-09 21:39:34 -0300

    tests: implementing controllers tests

--- 20-project-store-manager/test/unit/controllers/saleController.test.js
+++ 20-project-store-manager/test/unit/controllers/saleController.test.js
@@ -15,5 +15,52 @@ describe('test the sale controller layer', () => {
     }
   ];
   describe('test the getAll function', () => {
-    
+    before(() => {
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      sinon.stub(saleService, 'getAll').resolves(sale);
+    });
+    after(() => {
+      saleService.getAll.restore();
+    });
+
+    it('Requisition has status 200', async () => {
+      await saleController.getAll(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(response.json.calledWith(sinon.match.array)).to.be.equal(true);
+    });   
+  });
+
+  describe('test the getById function', () => {
+    before(() => {
+      request.params = {id: 1};
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      sinon.stub(saleService, 'getById').resolves(sale);
+    });
+    after(() => {
+      saleService.getById.restore();
+    });
+
+    it('Requisition has status 200', async () => {
+      await saleController.getById(request, response);
+      expect(response.status.calledWith(200)).to.be.true;
+      expect(response.json.calledWith(sinon.match.array)).to.be.true;
+    });
+  });
+
+  describe('test the getById function error', () => {
+    before(() => {
+      response.send = sinon.stub().returns();
+      sinon.stub(saleService, 'getById').resolves(false);
+    })
+    after(() => {
+      saleService.getById.restore();
+    });
+
+    it('Requisition has status 400', async () => {
+      await saleController.getById(request, response);
+      expect(response.status.calledWith(404)).to.be.true;
+    });
+  });
 });
