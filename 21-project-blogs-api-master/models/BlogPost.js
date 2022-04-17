20bfe3f150921dd3b4eb43e4695fc7852cee22b3
RiegerJulio <riegerproducoes@gmail.com>
2022-04-17 20:17:19 -0300

    feat: creating blogs models

---
+++ 21-project-blogs-api-master/models/BlogPost.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    title: { type: DataTypes.STRING, allowNull: false },
+    content: { type: DataTypes.TEXT, allowNull: false },
+    userId: { type: DataTypes.INTEGER, allowNull: false },
+    published: { type: DataTypes.DATE },
+    updated: { type: DataTypes.DATE },
+  }, { timestamps: false, tableName: 'BlogPosts' });
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      as: 'user',
+      foreignKey: 'userId' });
+  };
+  return BlogPost;
+};
