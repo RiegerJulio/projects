3abae637bf23a03c01dfd37b2de0ccde7df3c0c2
RiegerJulio <riegerproducoes@gmail.com>
2022-04-18 19:27:10 -0300

    refactor: refactoring userController to return error if user does not exist and pass req4

--- 21-project-blogs-api-master/controllers/userController.js
+++ 21-project-blogs-api-master/controllers/userController.js
@@ -27,6 +27,9 @@ const findById = async (req, res) => {
   try {
     const { id } = req.params;
     const user = await userService.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User does not exist' });
+    }
     return res.status(200).json(user);
   } catch (error) {
     return res.status(500).json({ message: error.message });
