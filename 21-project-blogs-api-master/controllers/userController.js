e6dd07ac3792acb4d199e7aadd3ebba0234c723c
RiegerJulio <riegerproducoes@gmail.com>
2022-04-18 17:23:09 -0300

    feat: userController

---
+++ 21-project-blogs-api-master/controllers/userController.js
@@ -0,0 +1,51 @@
+const userService = require('../services/userService');
+
+// createUser,
+// findAllUsers,
+// findById,
+// userLogin,
+
+const createUser = async (req, res) => {
+  try {
+    const user = await userService.createUser(req.body);
+    return res.status(201).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const findAllUsers = async (req, res) => {
+  try {
+    const users = await userService.findAllUsers();
+    return res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const findById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await userService.findById(id);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userService.userLogin(email, password);
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createUser,
+  findAllUsers,
+  findById,
+  userLogin,
+};
