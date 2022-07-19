39afa1144ee10b7c5371e5ded37a599a05098ad6
riegerjulio <riegerproducoes@gmail.com>
2022-07-19 18:19:22 -0300

    tests: implementing CarModel tests

---
+++ 27-project-car-shop/src/tests/unit/mocks/carMock.ts
@@ -0,0 +1,35 @@
+export const carMock = {
+  model: 'Ford Fiesta',
+  year: 2010,
+  color: 'Red',
+  buyValue: 10000,
+  seatsQty: 5,
+  doorsQty: 4,
+}
+
+export const carMockReturn = {
+  _id: '123451234512345123451234',
+  model: 'Ford Fiesta',
+  year: 2010,
+  color: 'Red',
+  buyValue: 10000,
+  seatsQty: 5,
+  doorsQty: 4,
+}
+
+export const validCarRequisitionMock = {
+  model: 'Ford Fiesta',
+  year: 2010,
+  color: 'Red',
+  buyValue: 10000,
+  seatsQty: 5,
+  doorsQty: 4,
+}
+
+export const invalidCarRequisitionMock = {
+  model: 'Ford Fiesta',
+  year: 2010,
+  color: 'Red',
+  buyValue: 10000,
+  seatsQty: 5,
+}
